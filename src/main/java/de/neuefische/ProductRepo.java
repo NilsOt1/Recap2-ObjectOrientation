1dea6895e8ddb165da055ce22a51a9a771689efa
nils <nilsotto@gmx.de>
2023-11-13 16:55:18 +0100

    add methods to ProductRepo

--- src/main/java/de/neuefische/ProductRepo.java
+++ src/main/java/de/neuefische/ProductRepo.java
@@ -2,7 +2,39 @@ package de.neuefische;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class ProductRepo {
     Map<Integer, Product> productMap = new HashMap<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductRepo that = (ProductRepo) o;
+        return Objects.equals(productMap, that.productMap);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productMap);
+    }
+
+    @Override
+    public String toString() {
+        return "ProductRepo{" +
+                "productMap=" + productMap +
+                '}';
+    }
+
+    public void addProduct(Product product, int productKey) {
+        productMap.put(productKey, product);
+    }
+
+    public void removeProduct(int productKey) {
+        productMap.remove(productKey);
+    }
+    public Product queryProduct(int productKey) {
+        return productMap.get(productKey);
+    }
 }
