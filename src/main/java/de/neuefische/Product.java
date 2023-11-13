2da8fbeb230b7d88d65fc196d0dd4815cadb04ee
nils <nilsotto@gmx.de>
2023-11-13 16:47:11 +0100

    add ProductRepo class and Product record

---
+++ src/main/java/de/neuefische/Product.java
@@ -0,0 +1,5 @@
+package de.neuefische;
+
+public record Product(String name, String id, Double price, boolean isAvailable) {
+
+}
---
+++ src/main/java/de/neuefische/ProductRepo.java
@@ -0,0 +1,8 @@
+package de.neuefische;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProductRepo {
+    Map<Integer, Product> productMap = new HashMap<>();
+}
